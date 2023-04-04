d46985b12487b51e0ef805815cf086e019a41252
SoumyaKaringula <128559244+SoumyaKaringula@users.noreply.github.com>
2023-04-04 15:56:35 -0400

    Update S3 bucket with upload file

--- s3bucket.tf
+++ s3bucket.tf
@@ -2,3 +2,11 @@ resource "aws_s3_bucket" "soumya109" {
   bucket = "soumya109"
   acl = "private"
 }
+
+#upload file
+resource "aws_s3_bucket_object" "upload" {
+  key        = "helloworld"
+  acl        = "private"
+  bucket     = aws_s3_bucket.soumya109.id
+  source     = "./helloworld.txt"
+}
